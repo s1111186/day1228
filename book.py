36f182e0420d6135612e9484e39d2d5dcf9f7264
s1111186 <s1111186@o365st.pu.edu.tw>
2023-12-20 14:30:59 +0800

    make it better

--- book.py
+++ book.py
@@ -9,30 +9,29 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
-app = Flask(__name__)
 
 @app.route("/")
 def index():
     X+="<a href=books>全部圖書</a><br>"
     X+="<a href=search>根據書名關鍵字查詢圖書</a><br>"
-    X+="<br><a href=spider>網路爬蟲擷取子青老師課程資訊</a><br><br>"
-    X+="<a href=/searchQ>查詢開眼電影即將上映影片</a><br>"
     return X
 
-
 @app.route("/books")
-def books():
-    Result = ""
-    db = firestore.client()
-    collection_ref = db.collection("圖書精選")
-    docs = collection_ref.order_by("anniversary").get()     
-    for doc in docs:
-        bk = doc.to_dict()         
-        Result += "書名： <a href=" +bk["url"] + ">" + bk["title"] + "</a><br>"
-        Result += "作者：" + bk["author"] + "<br>"
-        Result += str(bk["anniversary"]) + "週年紀念版" + "<br>"
-        Result += "<img src=" + bk["cover"] + "></img><br><br>"    
-    return Result
+def spider():
+    info = ""
+    url = "https://www.xbanxia.com/list/10_1.html"
+    Data = requests.get(url)
+    Data.encoding = "utf-8"
+    #print(Data.text)
+    sp = BeautifulSoup(Data.text, "html.parser")
+    result=sp.select(".team-box")
+
+    for x in result:
+        info += "<a href=" + x.find("a").get("href") + ">" + x.find("h4").text + "</a><br>"
+        info += x.find("p").text + "<br>"
+        info += x.find("a").get("href") + "<br>"
+        info +="<img src=https://www.xbanxia.com/" + x.find("img").get("src") + " width=200 height=300></img><br><br>"
+    return info
 
 @app.route("/search", methods=["GET", "POST"])
 def search():
@@ -52,26 +51,7 @@ def search():
                 Result += "<img src=" + bk["cover"] + "></img><br><br>"    
         return Result
     else:
-        return render_template("searchbk.html")
-
-
-@app.route("/searchQ", methods=["POST","GET"])
-def searchQ():
-    if request.method == "POST":
-        MovieTitle = request.form["MovieTitle"]
-        info = ""
-        db = firestore.client()     
-        collection_ref = db.collection("電影")
-        docs = collection_ref.order_by("showDate").get()
-        for doc in docs:
-            if MovieTitle in doc.to_dict()["title"]: 
-                info += "片名：" + doc.to_dict()["title"] + "<br>" 
-                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
-                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
-                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
-        return info
-    else:  
-        return render_template("input.html")
+        return render_template("search.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
