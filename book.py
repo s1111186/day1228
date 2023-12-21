90a26097b4edd91976d7a19a87f3bdaf6e42ab6c
s1111186 <s1111186@o365st.pu.edu.tw>
2023-12-21 10:09:57 +0800

    make it better

--- book.py
+++ book.py
@@ -12,36 +12,19 @@ from bs4 import BeautifulSoup
 
 @app.route("/")
 def index():
-    X+="<a href=books>全部圖書</a><br>"
     X+="<a href=searchb>根據書名關鍵字查詢圖書</a><br>"
     return X
-
-@app.route("/books")
-def books():
-    info = ""
-    url = "https://www.xbanxia.com/list/10_1.html"
-    Data = requests.get(url)
-    Data.encoding = "utf-8"
-    #print(Data.text)
-    sp = BeautifulSoup(Data.text, "html.parser")
-    result=sp.select(".team-box")
-
-    for x in result:
-        info += "<a href=" + x.find("a").get("href") + ">" + x.find("h4").text + "</a><br>"
-        info += x.find("a").get("href") + "<br>"
-        info +="<img src=https://www.xbanxia.com/" + x.find("img").get("src") + " width=200 height=300></img><br><br>"
-    return info
 
 @app.route("/searchQ", methods=["POST","GET"])
 def searchQ():
     if request.method == "POST":
-        MovieTitle = request.form["MovieTitle"]
+        NovelTitle = request.form["NovelTitle"]
         info = ""
         db = firestore.client()     
         collection_ref = db.collection("小說")
         docs = collection_ref.order_by("showDate").get()
         for doc in docs:
-            if MovieTitle in doc.to_dict()["title"]: 
+            if NovelTitle in doc.to_dict()["title"]: 
                 info += "書名：" + doc.to_dict()["title"] + "<br>" 
                 info += "作者：" + doc.to_dict()["author"] + "<br>"
                 info += "類型：" + doc.to_dict()["showLength"] + "<br>" 
